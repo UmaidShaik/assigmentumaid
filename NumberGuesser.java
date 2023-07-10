bcc028a17164a420fa117e00a2269c85f3e8a16d
Umaid Shaikh <Umaid.Shaikh@asl.mum.sst>
2023-07-10 21:56:45 +0530

    numberguesser assignment

---
+++ NumberGuesser.java
@@ -0,0 +1,47 @@
+package assignment;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuesser {
+
+	public static void main(String[] args) {
+		
+		int turns=1;
+		double Number=1;
+		int result=0;
+        
+		Scanner sc=new Scanner(System.in);
+		Random rand=new Random();
+		int randomnumber=rand.nextInt(100);
+		while(turns<=6)
+		{
+			System.out.println("your turn is "+ turns );
+			System.out.println("enter the Number ");
+			Number=sc.nextDouble();
+			if(randomnumber==Number) {
+				System.out.println("You win");
+				result=1;
+				break;
+			}
+			else if(randomnumber<Number){
+				System.out.println("Number is too high...Enter the small Number");
+			}
+			else if(randomnumber>Number) {
+				System.out.println("Number is too low...Enter the high Number");
+			}
+			else {
+				System.out.println("Invalid Input");
+			}
+			turns++;
+		}
+		
+		if(result==0) {
+			System.out.println("you lose ");
+			System.out.println("your number is "+ randomnumber);
+		}
+		
+	}
+
+}
+
