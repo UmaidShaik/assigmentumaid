8b6d80a144d6f4bcc085a6b26c6ba7281eb1fedc
Umaid Shaikh <Umaid.Shaikh@asl.mum.sst>
2023-08-07 00:24:46 +0530

    Car Inherit

--- Car/VehicleTest.java
+++ Car/VehicleTest.java
@@ -4,9 +4,10 @@ public class VehicleTest {
 
 	public static void main(String[] args) {
 		Car cars[] = {
-			new Car("Qwerty", "BK186", "Blue", 120, 25, 120000),
-			new Car("Budgri", "618-RHR", "Red", 90, 20, 10000),
-			new Car("Lamonson", "OIO45", "Black", 150, 27, 190000),
+			new Car("Audi", "A-97", "Blue", 130, 55, 150000),
+			new Car("Mercedes", "M-895", "Red", 90, 20, 10000),
+			new Car("Land rover", "OIO45", "Black", 150, 27, 130000),
+			new Car("TATA","A-0989","Green",160,32,140000),
 		};
 		
 		for (int i=0; i<cars.length; i++) {
